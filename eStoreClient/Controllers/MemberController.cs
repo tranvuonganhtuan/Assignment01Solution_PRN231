3efda8cfdc6279b9a8bda20495a6bd9a9da1b2f5
AnhTuanTran <90033290+tranvuonganhtuan@users.noreply.github.com>
2023-09-23 11:04:08 +0700

    aaaaaa
    aaaaa

---
+++ eStoreClient/Controllers/MemberController.cs
@@ -0,0 +1,35 @@
+﻿using BusinessObject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace eStoreClient.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+        private readonly HttpClient client = null;
+        private string MemberApiUrl = "";
+        public MemberController() { 
+
+            client = new HttpClient();
+            var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentType);
+            MemberApiUrl = "https://localhost:44314/api/products";
+        }
+        public async Task<IActionResult> Index()
+        {
+            HttpResponseMessage response = await client.GetAsync(MemberApiUrl);
+            string strData = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            List<Member> listMembers = JsonSerializer.Deserialize<List<Member>>(strData, options);
+            return View(listMembers);
+        }
+    }
+}
