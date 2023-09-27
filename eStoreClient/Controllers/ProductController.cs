772c78df6379f80acb38899260f55f1d42f43e63
AnhTuanTran <90033290+tranvuonganhtuan@users.noreply.github.com>
2023-09-27 09:41:07 +0700

    aaa
    aaaa

---
+++ eStoreClient/Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿using BusinessObject.Models;
+using eStoreClient.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace eStoreClient.Controllers
+{
+    public class ProductController : Controller
+    {
+        Uri baseAddress = new Uri("https://localhost:44314/api");
+        private readonly HttpClient _httpClient;
+
+        public ProductController()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = baseAddress;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            try
+            {
+                List<Product> productList = new List<Product>();
+                HttpResponseMessage response = _httpClient.GetAsync("product/Get").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string data = response.Content.ReadAsStringAsync().Result;
+                    productList = JsonConvert.DeserializeObject<List<Product>>(data);
+                }
+                else
+                {
+                    // Log the error
+                    Console.WriteLine("Error accessing the API: " + response.ReasonPhrase);
+                    // You might want to log this error in a file or a logging system
+                }
+
+                return View(productList);
+            }
+            catch (Exception ex)
+            {
+                // Log the error
+                Console.WriteLine("An error occurred: " + ex.Message);
+                return View(new List<Product>());
+            }
+        }
+    }
+}
